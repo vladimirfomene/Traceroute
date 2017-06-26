236dacd158bf12200fd5581aa1643c7af552cbec
Vladimir Fomene <vladimirfomene@gmail.com>
2017-06-26 05:30:01 +0100

    Create traceroute homepage

---
+++ index.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+  <head>
+    <title>Traceroute-Online</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <script src="js/script.js" type="text/js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="banner">Traceroute Online</div>
+      <div class="content">
+        <div class="user-form">
+          <form method="get" action="">
+            <input type="text" name="domain" placeholder="www.yourdomain.com"><br>
+            <input type="submit" name="traceroute">
+          </form>
+        </div>
+        <div class="gmap-result"></div>
+      </div>
+      <div class="footer">
+      </div>
+    </div>
+  </body>
+</html>
