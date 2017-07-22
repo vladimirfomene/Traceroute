e69aa775a94ea3fed15cde1df77900f7c6b16d13
Vladimir Fomene <vladimirfomene@gmail.com>
2017-07-22 13:26:15 +0000

    Create for js code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+
+console.log("Hello there.");
+/* This codes sends a request to the server to get the ip addresses of the route */
+var domaintxtField = document.getElementById("txtDomain");
+var form = document.querySelector("form");
+console.log(domaintxtField.value);
+form.addEventListener("submit", function(event){
+  var req = ajaxRequest();
+  console.log(domaintxtField.value);
+  req.open("GET", "traceroute.php?domain=" + domaintxtField.value, true);
+  req.addEventListener("load", function() {
+    if(req.status == 200 && req.readyState == 4){
+      var Ips = JSON.parse(req.responseText);
+      console.log(Ips);
+      Ips.forEach(function(ip){
+        var resultArea = document.querySelector(".gmap-result");
+        var ipTxt = document.createTextNode(ip);
+        resultArea.appendChild(ipTxt);
+      });
+    }
+  });
+  req.send(null);
+  event.preventDefault();
+});
+
 /* Creates a XMLHttpRequest request object for recent and old browsers */
 function ajaxRequest()
 {
